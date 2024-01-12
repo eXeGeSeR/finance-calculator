d50937babbd0f98fd84344c87a59e421dd02a316
eXeGeSeR <cosminandone@yahoo.co.uk>
2024-01-12 16:21:21 +0000

    Update finance_calculators.py

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,7 +1,12 @@
 import math
 
 # Ask the user about the type of calculation. Make it case insensitive!
-type_calculation = input("What type of calculation would you like?\n").casefold()
+type_calculation = input('''What type of calculation would you like?\n
+                        1. Would you like an investment -to calculate the amount 
+                        of the interest you will earn on your investment?\n
+                        2. Would you like a home loan bond -to calculate the amount 
+                        you'll have to pay on a home loan?\n
+                        Enter either "investment" or "bond" from the menu above to proceed \n''').casefold()
 
 # Creating an if-elif-else structure based on user's answer
 # Investment -calculation of the total of money accumulated with the interest
@@ -11,7 +16,7 @@ if type_calculation == "investment":
 
     # Ask the user the amount of money available to invest, the interest rate, the number of years and the type of interest
     invested_money = int(input("What is the amount of money do you like to invest?\n"))
-    interest_rate = float(input("What interest rate are you interested on?\n"))
+    interest_rate = float(input("What interest rate are you interested on? Please type only digits, no need for % symbol!\n"))
     years = int(input("How many years would you like to invest in?\n"))
     interest_type = str(input("What kind of interest rate do you prefer: simple or compound?\n")).casefold()
 
@@ -36,11 +41,11 @@ elif type_calculation == "bond":
     repayment = 0
     # Ask the user to type the present value of the house, the interest rate and the number of the months
     house_price = int(input("What is the present value of the house?\n"))
-    interest_rate = float(input("What is the monthly interest rate?\n"))
-    months = int(input("How many months would you like to make the repayment?\n")) 
+    interest_rate = float(input("What is the interest rate? Please type only digits, no need for the % symbol!\n"))
+    months = int(input("How many months would you like to make the repayment?\n"))
 
     # Calculate the monthly repayment of the house and print it out
-    repayment = (interest_rate * house_price)/(1 - (1 + (interest_rate/100))**(-months))/months
+    repayment = ((interest_rate/100/12) * house_price)/(1 - (1 + (interest_rate/100/12))**(-months))
     print(f"The monthly repayment of the house is £{repayment}.")
 
 else: 
